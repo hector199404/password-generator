513ba35d222955a2fbff65774b36b4f1075ab25e
Hector Nahuel Lucero <hectorlucero1994@gmail.com>
2023-05-06 16:56:39 -0300

    Update password_encrypted.py

--- password_encrypted.py
+++ password_encrypted.py
@@ -7,9 +7,9 @@ num = "0123456789"
 simb = "/*+-_$&%."
 
 base = minus+mayus+num+simb
-longitud = 15
+longitud = 15 # Here we set the length of our password
 
-for _ in range(100):
+for _ in range(100): # Here we establish how many passwords we want to generate
         muestra = random.sample(base, longitud)
         password ="".join(muestra)
         password_encr = generate_password_hash(password)
